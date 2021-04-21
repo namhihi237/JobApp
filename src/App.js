d36b1ed1d1e544c63f0078cd43b23e06c959593c
namhihi237 <yentth237@gmail.com>
2021-04-21 19:37:05 +0700

    fix merger

--- src/App.js
+++ src/App.js
@@ -37,9 +37,7 @@ const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const CompanyPostStack = createStackNavigator();
-
 const IterStack = createStackNavigator();
-
 
 class C3 extends Component {
   render() {
@@ -72,13 +70,29 @@ class SettingDrawer extends Component {
             </DrawerContentScrollView>
           );
         }}>
-        <Drawer.Screen name="Change password" component={changePass} />
+        <Drawer.Screen name="C2" component={changePass} />
         <Drawer.Screen name="C3" component={C3} />
       </Drawer.Navigator>
     );
   }
 }
 
+class IterNav extends Component {
+  render() {
+    return (
+      <IterStack.Navigator initialRouteName="Loading">
+        <Stack.Screen
+          name="My CV"
+          component={getOneCv}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen name="CreateCv" component={Cv} />
+      </IterStack.Navigator>
+    );
+  }
+}
 class tabBarForIter extends Component {
   render() {
     return (
@@ -99,7 +113,7 @@ class tabBarForIter extends Component {
           inactiveTintColor: 'gray',
         }}>
         <Tab.Screen name="Home" component={Job} />
-
+        <Tab.Screen name="My CV" component={IterNav} />
         <Tab.Screen name="Setting" component={SettingDrawer} />
       </Tab.Navigator>
     );
